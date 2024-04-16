252a09f6e29ce3606204830f3d18bf1b8a6fabdb
Jon Harris <jonathan.harris@mlprograms.com>
2024-04-16 17:12:34 +0200

    [config] Add version control packages

--- init.el
+++ init.el
@@ -72,3 +72,30 @@
   (completion-styles '(orderless basic))
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package magit
+  :bind ("C-x g" . magit-status)
+  :diminish magit-minor-mode
+  :hook (git-commit-mode . (lambda () (setq fill-column 72)))
+  :mode ("/\\.gitmodules\\'" . conf-mode)
+  :custom
+  ;; hide ^M chars at the end of the line when viewing diffs
+  (magit-diff-hide-trailing-cr-characters t)
+
+  ;; Limit legth of commit message summary
+  (git-commit-summary-max-length 50)
+
+  ;; Open status buffer in same buffer
+  (magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package git-gutter
+  :hook (prog-mode org-mode)
+  :bind (("C-x P" . git-gutter:previous-hunk)
+	 ("C-x N" . git-gutter:next-hunk)
+	 ("C-x G" . git-gutter:popup-hunk))
+    :config
+    ;; Must include if 'linum-mode' activated (common in 'prog-mode')
+    ;; because 'git-gutter' does not work with 'linum-mode'.
+    (use-package git-gutter-fringe
+      :commands git-gutter-mode
+      :config (global-git-gutter-mode)))
