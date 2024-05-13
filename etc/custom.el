5365042c78f1ccb7fde555ae9f8a70bcc32cb7b4
Jon Harris <jonathan.harris@mlprograms.com>
2024-05-13 12:55:36 +0200

    Configure org-capture-templates

---
+++ etc/custom.el
@@ -0,0 +1,21 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mlp-members
+   '("ALL" "DEVS" "Damian" "James" "Mike" "Bon" "David" "Luke" "Cristina" "Baran" "Fabrizio" "Chris" "Nestor" "Seva" "Berke") t)
+ '(mlp-projects nil t)
+ '(mlp-repos '("other_thing" "ogi_products") t)
+ '(mlp-text '("cat" "dog" "bird") t nil "List of repositories used at MLP")
+ '(my-work-projects '("1_base_model") t)
+ '(my-work-repos
+   '("mlp-docs" "mlp-emacs-config" "af-robovisor" "ogi_products") t)
+ '(package-selected-packages
+   '(helpful org-ql org-super-agenda projectile org-agenda company yankpad consult org-appear org-modern iscroll highlight-thing beacon doom-modeline doom-themes git-gutter-fringe magit git-gutter vertico no-littering)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
